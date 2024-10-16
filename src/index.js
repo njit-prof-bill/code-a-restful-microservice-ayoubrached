9abb6ff816b4ee852cd466cf685e3e15c1ae2b49
Ayoub Rached <Ayoub@MacBook-Air-2.local>
2024-10-16 17:54:03 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,45 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+const users = [];
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.param('id', function(req, res, next, id) {
+    const user = users.find(u => u.id == id);
+    if (!user) {
+        return res.status(404).send({ error: 'User not found' });
+    }
+    req.user = user;
+    next();
+  });
+
+app.post('/users', (req, res) => {
+    const user = {
+        "name": req.body.name,
+        "email": req.body.email,
+        "id": Math.floor(Math.random() * 100)
+    };
+    users.push(user);
+    console.log(user);
+    res.status(201).send(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const user = req.user;
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    res.send(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id == req.user.id);
+    if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+    }
+    res.status(204).send();
+});
+
+app.get('/users/:id', (req, res) => {
+    res.send(req.user);
 });
 
 // Do not touch the code below this comment
@@ -22,5 +58,6 @@ if (process.env.NODE_ENV !== 'test') {
         console.log(`Server running at http://localhost:${port}`);
     });
 }
+
 
 module.exports = app; // Export the app for testing
